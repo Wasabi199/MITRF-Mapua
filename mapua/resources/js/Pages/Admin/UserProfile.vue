862b703d610febece032640f000aff4da42d905c
lourenzs <75241980+lourenzs@users.noreply.github.com>
2022-06-25 21:39:51 +0800

    UserProfile updated

--- mapua/resources/js/Pages/Admin/UserProfile.vue
+++ mapua/resources/js/Pages/Admin/UserProfile.vue
@@ -1,14 +1,96 @@
 <template>
-<h1>{{user.name}}</h1>
-    
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                User Profile
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden border-gray-300 shadow-xl sm:rounded-lg">
+
+                    <!-- Name -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Name: </p>
+                        <p class="ml-2 underline">{{user.name}}</p>
+                    </div>
+                    <!-- address -->
+
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Address:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- contact number -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Contact Number:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- department -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Department:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- membership date -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Member Since:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- Contribution -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Current Contribution:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- current loan -->
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Current loan:</p>
+                        <p class="ml-2 underline"></p>
+                    </div>
+                        <!-- past loans -->
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <thead class="inline-flex">
+                                        <tr class="m-2">
+                                            <th class="text-left px-16">Past Loan</th>
+                                            <th class="text-left px-16">Amount</th>
+                                            <th class="text-left px-16">Terms</th>
+                                            <th class="text-left px-16">Date of Completion</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </AppLayout>
+
 </template>
 <script>
+
+import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
 export default {
+    components:{
+        JetApplicationLogo,
+        AppLayout,
+    },
     setup() {
-        
+
     },
     props:{
-        user:Object
+        user:Object,
+        address:Object,
     }
 }
 </script>
--- mapua/resources/js/Pages/Admin/Users.vue
+++ mapua/resources/js/Pages/Admin/Users.vue
@@ -35,19 +35,19 @@
                                 </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y divide-gray-200">
-                                    </tbody>
-                                    </table>
-                                    </div>
-                                    </div>
-                                    </div>
-                                    </div>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         <div class="flex flex-col ">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
-                        
+
                             <tbody class="bg-white divide-y divide-gray-200">
 
                                 <tr v-for="user in users.data" v-bind:key="user.id">
@@ -196,7 +196,7 @@ export default {
     props: {
         filters: Object,
         users: Object,
-       
+
     },
 
 
