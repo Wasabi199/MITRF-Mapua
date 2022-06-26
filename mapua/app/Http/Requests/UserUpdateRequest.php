ac94e81d05ed7557785382c7e4a579808d201156
Wasabi199 <xxryan0909@gmail.com>
2022-06-26 15:33:11 +0800

    Create UserUpdateRequest.php

---
+++ mapua/app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::user()->userType ==1;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'id'=>'required|exists:admins,id',
+            'first_name' => 'required',
+            'middle_name' => 'required',
+            'last_name' => 'required',
+            
+        ];
+    }
+}
