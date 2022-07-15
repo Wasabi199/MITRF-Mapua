01b888d9878166b782898d0eeb3662a4ca053bcd
MatthewB88 <johnmatthew.borja@gmail.com>
2022-07-15 16:08:35 +0800

    Update UserLoanPage.vue

--- mapua/resources/js/Pages/Users/UserLoanPage.vue
+++ mapua/resources/js/Pages/Users/UserLoanPage.vue
@@ -188,9 +188,71 @@
         <!-- Housing Loan Guidelines -->
         <div v-if="page == 'Housing Loan'" class="p-5">
             <div class="flex justify-center text-xl font-bold text-gray-900 my-3">
-                <span>Guideline : {{page}}</span>
-             
+                <span>Guideline : {{page}} 
+                </span>
             </div>
+            <br>
+            <div class="flex justify-left text-l font-bold text-gray-900 my-3">
+                <span><u>MITRF LOAN TERMS AND CONDITIONS</u>
+                </span>
+            </div>
+            <br>
+            <div class="flex justify-left text-m font-bold text-gray-900 my-3">
+                <span>Eligibility Requirements
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>1. Member-borrower must be a regular employee of the institute and contributing 5% of his monthly basic salary to MITRF.
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>2. Member-borrower must maintain at least 45% net take home pay after deducting all his obligations including the MITRF amortization.
+                </span>
+            </div>
+            <br>
+            <div class="flex justify-left text-m font-bold text-gray-900 my-3">
+                <span>Loan Amount
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>1. The maximum amount shall be the total member-follower's fund contribution and earnings.
+                </span>
+            </div>
+            <br>
+            <div class="flex justify-left text-m font-bold text-gray-900 my-3">
+                <span>Housing Assistance
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>For housing, Loan requirements are the following: (30,000 - 100,000 maximum, 1-2 years term)
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>1. Application Form
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>2. Photo of you including the place to be improved/repaired.
+                </span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span>3. Laborer's quotation.
+                </span>
+            </div>
+            <br>
+            <div class="flex justify-left text-s text-gray-900 my-3">
+                <span><u><i>This loan have a 9% interest per annum and a 2% service fee</i></u>
+                </span>
+            </div>
+            <div class="flex justify-left text-s font-bold text-gray-900 my-3">
+                <span><u>Note: It cannot be loaned at the same time with Education Loan.</u>
+                </span>
+            </div>
+
+
+
+
+
             <div>
 
             </div>
