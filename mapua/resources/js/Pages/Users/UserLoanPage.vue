93770b9ff81b2694964c3ff2637e16175cadd1d8
Mrcotan <78008498+Mrcotan@users.noreply.github.com>
2022-07-16 12:21:59 +0800

    added medical

--- mapua/resources/js/Pages/Users/UserLoanPage.vue
+++ mapua/resources/js/Pages/Users/UserLoanPage.vue
@@ -9,12 +9,12 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="ml-5">
+                    <div class="ml-20 mt-10">
                         <p class="mb-1 mt-4 text-lg">Purpose of Loan</p>
                         <select
                             v-model="form.loan_type" 
                             @change="showModal = !showModal"
-                            class="mb-5  border-2 border-gray-400 border-opacity-50 hover:border-indigo-500 transition ease-in duration-150 rounded-md ">
+                            class="border-2 border-gray-400 border-opacity-50 hover:border-indigo-500 transition ease-in duration-150 rounded-md ">
                             <option value="placeholder" disabled>Select Loan Type</option>
                             <option  v-for="type in loanTypes" v-bind:key="type" :value="type">{{type}}</option>
 
@@ -352,12 +352,124 @@
 
           <!-- Medical Reimbursement Loan Guidelines -->
         <div v-if="form.loan_type == 'Medical Reimbursement'" class="p-5">
-            <div class="flex justify-center text-xl font-bold text-gray-900 my-3">
+            <div class="flex justify-center text-xl font-bold text-gray-900 my-3 mb-12">
                 <span>Guideline : {{form.loan_type}}</span>
               
             </div>
-            <div>
+            <div class="flex justify-left text-l font-bold text-gray-900 my-3 mb-12">
+                <span><u>MITRF LOAN TERMS AND CONDITIONS</u></span>
+            </div>
+            <div class="flex justify-left text-m font-bold text-gray-900 my-3">
+                <span>Note: In-patient and out-patient medical benefits of MITRF members are subject to the conditions found in the attached Appendix.</span>
+            </div>
+            <div class="flex justify-left text-s text-gray-900 mb-9">
+                <ol>
+                    <li class="mb-3">1. This benefit is available to all members of the MITRF only. No contestability period is required to avail of the benefit.</li>
+                    <li class="my-3">2. Membership starts upon submission of the MITRF Office of the duly accomplished Information Sheet of Members.</li>
+                    <li class="my-3">3. A member who was confined for at least one day is entitled to hospitalization benefit equivalent to the actual expenses incurred but should not exceed Php 12,000.00 per year. The member is required to submit any document from the hospital or doctor which shows or proves actual hospitalization subject to the requirements under Section 8 and 9.</li>
+                    <li class="my-3">4. In case of procedures or treatment which does not require hospitalization but which entitle the member to claim SSS or Philheath in-patient medical benefits, the member is also entitled to claim the in-patient benefit provided the member submits documents showing that he has undergone such procedure or treatment (i.e hospital or clinical records, doctor's records, reports or request, billings or receipts).</li>
+                    <li class="my-3">5. A member can be considered as an in-patient if treated for all kinds of sickness and he/she had undergone any surgery except aesthetic surgery.</li>
+                    <li class="my-3">6. A member can also avail of the medical reimbursement for out-patient medical, vision correction, dental procedures (doctor's fee, prescription contact lens and eyeglass, prophylaxis, extraction, filling, laboratory fees, drugs and medicines including P500 worth of vitamins and food supplements) up to the maximum amount of Php 7,000.00 per year. This amount is deductible from the total hospitalization benefit due to members upon presentation of OR of doctor's fee, laboratory request, doctor's prescription and OR of all the medicines and laboratory procedures.</li>
+                    <li class="my-3">7. A member is required to present the original official receipt and submit photocopy of the original receipt duly signed by the MITRF Office Manager.</li>
+                    <li class="my-3">8. Members who have HMO cards should present a duly certified billing statement from the hospital/clinic.</li>
+                    <li class="my-3">9. Request for reimbursement must be made within (3) months from the date the expenses were incurred. Submission beyond the three-month period shall no longer be reimbursed.</li>
+                </ol>
+            </div>
+            <div class="flex justify-left text-m font-bold text-gray-900 italic my-3 mb-3">
+                <span>Appendix:</span>
+            </div>
+            <div class="flex justify-left text-m font-bold text-gray-900 underline my-3 mb-5">
+                <span>A. Computation of Medical benefits for 2021 Members of MITRF</span>
+            </div>
 
+            <table class="table-auto m-auto bg-white shadow rounded-lg w-5/6">
+                <thead class="bg-blue-100">
+                    <tr>
+                        <th>Membership Year</th>
+                        <th>Medical Benefits</th>                   
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr>
+                        <td>1 year &le; x &#60; 2 years</td>
+                        <td>10%</td>
+                    </tr>
+                    <tr>
+                        <td>2 years &le; x &#60; 3 years</td>
+                        <td>20%</td>
+                    </tr>
+                    <tr>
+                        <td>3 years &le; x &#60; 4 years</td>
+                        <td>30%</td>
+                    </tr>
+                    <tr>
+                        <td>4 years &le; x &#60; 5 years</td>
+                        <td>40%</td>
+                    </tr>
+                    <tr>
+                        <td>5 years &le; x &#60; 6 years</td>
+                        <td>50%</td>
+                    </tr>
+                    <tr>
+                        <td>6 years &le; x &#60; 7 years</td>
+                        <td>60%</td>
+                    </tr>
+                    <tr>
+                        <td>7 years &le; x &#60; 8 years</td>
+                        <td>70%</td>
+                    </tr>
+                    <tr>
+                        <td>8 years &le; x &#60; 9 years</td>
+                        <td>80%</td>
+                    </tr>
+                    <tr>
+                        <td>9 years &le; x &#60; 10 years</td>
+                        <td>90%</td>
+                    </tr>
+                    <tr>
+                        <td>x &ge; 10 years</td>
+                        <td>100%</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="text-m text-gray-900 italic my-3 mb-10">
+                <span class="float-right pr-12 underline">Where x = years as member of MITRF</span><br>
+            </div>
+
+            <div class="flex justify-left text-m font-bold text-gray-900 underline my-3 mb-5">
+                <span>B. Computation of Medical benefits prior to 2021</span>
+            </div>
+
+            <table class="table-auto m-auto bg-white rounded-lg  shadow w-5/6">
+                <thead class="bg-blue-100">
+                    <tr>
+                        <th>Membership Year</th>
+                        <th>Medical Benefits</th>                   
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <tr>
+                        <td>1 year &le; x &#60; 3 years</td>
+                        <td>50%</td>
+                    </tr>
+                    <tr>
+                        <td>3 year &le; x &#60; 5 years</td>
+                        <td>70%</td>
+                    </tr>
+                    <tr>
+                        <td>x &ge; 5 years</td>
+                        <td>100%</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="text-m text-gray-900 italic my-3 mb-10">
+                <span class="float-right pr-12 underline">Where x = years as member of MITRF</span><br>
+            </div>
+
+            <div class="text-m text-gray-900 italic my-3 mb-10">
+                <span class="pr-12 underline">*The MITRF Board reserves the right to review outpatient medical benefits annually, as deemed relevant by the Board.</span><br>
             </div>
             <div class="flex justify-center text-xl font-bold dark:text-gray-200 my-3 space-x-40">
               
