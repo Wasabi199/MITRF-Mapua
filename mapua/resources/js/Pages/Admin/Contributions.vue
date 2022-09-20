9271921ace4b33b09b30aca6b40a91253c2268f4
Arien Agcanas <agcanas.arien@gmail.com>
2022-09-20 14:14:26 +0800

    Added For Release and Released UI
    + Release text placeholder and Released button in "For Release" Stage

--- mapua/resources/js/Pages/Admin/Contributions.vue
+++ mapua/resources/js/Pages/Admin/Contributions.vue
@@ -16,6 +16,7 @@
         </template>
      
     <div class="grid grid-cols-1 m-8 gap-8 lg:m-10 lg:grid-cols-2   ">
+        <!-- Left Box -->
         <div class="bg-white overflow-hidden border-gray-300 shadow-xl rounded-lg">            
             <div v-if="loan.approval === 'Submitted'">
                 <form>
@@ -83,8 +84,9 @@
                     </div>
                     </Link>
                 </div>
-                </div>
-                <div v-if="loan.approval == 'Processed'" class="bg-white p-4 overflow-hidden border-gray-300 shadow-xl sm:rounded-lg ">
+            </div>
+
+            <div v-if="loan.approval == 'Processed'" class="bg-white p-4 overflow-hidden border-gray-300 shadow-xl sm:rounded-lg ">
 
                         <!-- Contact -->
                         <div class="mb-8">
@@ -176,8 +178,9 @@
                                 }}</p> 
                         </div> -->
                         
-                </div>
-                <div v-if="loan.approval === 'For Processing'">
+            </div>
+
+            <div v-if="loan.approval === 'For Processing'">
                 <form>
                     <div class="px-2 py-5 bg-white sm:p-6">
                         <div class="grid grid-cols-6 gap-6">
@@ -243,24 +246,177 @@
                     </div>
                     </Link>
                 </div>
-                </div>
-                
             </div>
-        <!-- <div v-if="loan.approval == 'Reviewing' ">     
-            
-                <div class="bg-white shadow-xl rounded-lg content-center lg:mr-50 "> 
-                    <img :src="loan.attachment_path == null ? '' : loan.attachment_path" class="w-auto h-auto" >
+
+            <div v-if="loan.approval == 'For Release'" class="bg-white p-4 overflow-hidden border-gray-300 shadow-xl sm:rounded-lg ">
+                <div class="mb-8">
+                    <div class="flex text-lg">
+                        <svg class="h-6 w-6 text-red-900" fill="none" stroke="currentColor" stroke-width="2"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                        <p class="pl-2 font-semibold">{{ info.mobile_number }}</p>
+                    </div>
                 </div>
+
+                <!-- Name -->
+                <div class="justify-between flex">
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Name: </p>
+                        <p class="ml-2 underline">{{ info.first_name }} {{info.last_name}}</p>
+                    </div>
+
+                </div>
+
+                <!-- Civil status -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Civil Status:</p>
+                    <p class="ml-2 underline">{{info.civil_status}}</p>
+                </div>
+
+                <!-- birth date -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Birthdate:</p>
+                    <p class="ml-2 underline">{{info.birth_date}}</p>
+                </div>
+
+                <!-- place of birth -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Place of Birth:</p>
+                    <p class="ml-2 underline">{{info.birth_place}}</p>
+                </div>
+                <!-- address -->
+
+                <!-- <div class="flex text-lg">
+                    <p class="font-semibold">Address:</p>
+                    <p class="ml-2 underline">{{ user.admin_reg.current_address }} {{ user.admin_reg.barangay }}
+                        {{ user.admin_reg.municipality }}</p>
+                </div> -->
+
+                <!-- department -->
+
+                <div class="flex text-lg">
+                    <p class="font-semibold">Department:</p>
+                    <p class="ml-2 underline">{{ info.department }}</p>
+
+                </div>
+                <!-- membership date -->
+
+                <div class="flex text-lg">
+                    <p class="font-semibold">Member Since:</p>
+                    <p class="ml-2 underline">{{ info.membership}}</p>
+
+                </div>
+                <!-- Contribution -->
+                <!-- <div class="flex text-lg">
+                    <p class="font-semibold">Current Contribution:</p>
+                <p class="ml-2 underline"> 
+                        {{
+                            user.admin_reg.contribution != null ? user.admin_reg.contribution : 'No Contribution'
+                        }}</p> 
+                </div> -->
+
+            </div>
+
+            <div v-if="loan.approval == 'Released'" class="bg-white p-4 overflow-hidden border-gray-300 shadow-xl sm:rounded-lg ">
+                <div class="mb-8">
+                    <div class="flex text-lg">
+                        <svg class="h-6 w-6 text-red-900" fill="none" stroke="currentColor" stroke-width="2"
+                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                        <p class="pl-2 font-semibold">{{ info.mobile_number }}</p>
+                    </div>
+                </div>
+
+                <!-- Name -->
+                <div class="justify-between flex">
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Name: </p>
+                        <p class="ml-2 underline">{{ info.first_name }} {{info.last_name}}</p>
+                    </div>
+
+                </div>
+
+                <!-- Civil status -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Civil Status:</p>
+                    <p class="ml-2 underline">{{info.civil_status}}</p>
+                </div>
+
+                <!-- birth date -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Birthdate:</p>
+                    <p class="ml-2 underline">{{info.birth_date}}</p>
+                </div>
+
+                <!-- place of birth -->
+                <div class="flex text-lg">
+                    <p class="font-semibold">Place of Birth:</p>
+                    <p class="ml-2 underline">{{info.birth_place}}</p>
+                </div>
+                <!-- address -->
+
+                <!-- <div class="flex text-lg">
+                    <p class="font-semibold">Address:</p>
+                    <p class="ml-2 underline">{{ user.admin_reg.current_address }} {{ user.admin_reg.barangay }}
+                        {{ user.admin_reg.municipality }}</p>
+                </div> -->
+
+                <!-- department -->
+
+                <div class="flex text-lg">
+                    <p class="font-semibold">Department:</p>
+                    <p class="ml-2 underline">{{ info.department }}</p>
+
+                </div>
+                <!-- membership date -->
+
+                <div class="flex text-lg">
+                    <p class="font-semibold">Member Since:</p>
+                    <p class="ml-2 underline">{{ info.membership}}</p>
+
+                </div>
+                <!-- Contribution -->
+                <!-- <div class="flex text-lg">
+                    <p class="font-semibold">Current Contribution:</p>
+                <p class="ml-2 underline"> 
+                        {{
+                            user.admin_reg.contribution != null ? user.admin_reg.contribution : 'No Contribution'
+                        }}</p> 
+                </div> -->
+
+            </div>                  
+            </div>
             
+        <!-- Right Box -->
+            <div v-if="loan.approval == 'Released'" class="bg-white shadow-xl rounded-lg content-center lg:mr-50 ">
+               <div class="justify-between flex">
+                    <div class="flex text-lg">
+                        <p class="font-semibold">Location to recieve: </p>
+                        <p class="ml-2 underline">Test</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="bg-white shadow-xl rounded-lg content-center lg:mr-50 ">
+                <img :src="loan.attachment_path == null ? '' : loan.attachment_path" class="w-auto h-auto" >
+            </div> -->
 
             <br>
-            <div  class="w-full p-0 m-0 flex items-center justify-center gap-20">
+            <!-- <div  class="w-full p-0 m-0 flex items-center justify-center gap-20">
                     <button  type="button" class=" py-2 px-4 mb-5 bg-red-600 hover:bg-red-700 focus:ring focus:ring-red-300 text-white w-64 
                         transition ease-in duration-150 text-lg text-center font-semibold shadow-md rounded-lg"  @click="rejectLoan(loan)">Reject</button>
  
                     <button type="button" class=" py-2 px-4 mb-5 bg-green-600 hover:bg-green-700 focus:ring focus:ring-green-300 text-white w-64 
                         transition ease-in duration-150 text-lg text-center font-semibold shadow-md rounded-lg"  @click="acceptLoan(loan)">Accept</button>
-            </div>
+            </div> -->
         </div> -->
 
         <div v-if="loan.approval == 'Submitted'" class="row-span-3  ">     
@@ -286,6 +442,7 @@
                         transition ease-in duration-150 text-lg text-center font-semibold shadow-md rounded-lg"  @click="acceptLoan(loan)">Accept</button>
             </div> -->
         </div>
+
         <div v-if="loan.approval == 'Processed'" class="row-span-3 ">     
             
                 <div class="bg-white shadow-xl rounded-lg  content-center lg:ml-20 w-80 h-auto "> 
@@ -309,7 +466,8 @@
                         transition ease-in duration-150 text-lg text-center font-semibold shadow-md rounded-lg"  @click="acceptLoan(loan)">Accept</button>
             </div> -->
         </div>
-          <div v-if="loan.approval == 'For Processing'" class="row-span-3">     
+
+        <div v-if="loan.approval == 'For Processing'" class="row-span-3">     
             
                 <div class="bg-white shadow-xl rounded-lg content-center lg:mr-50  "> 
                     <img :src="loan.attachment1 == null ? '' : loan.attachment1" class="w-auto h-auto" >
@@ -331,7 +489,37 @@
             </div>
    
         </div>
-  
+
+        <div v-if="loan.approval == 'For Release'" class="row-span-3">     
+            <div class="bg-white shadow-xl rounded-lg  content-center lg:ml-20 w-80 h-auto "> 
+                <img :src="loan.attachment1 == null ? '' : loan.attachment1" class="w-auto h-auto" >
+            </div>
+                <div class="bg-white shadow-xl rounded-lg content-center lg:ml-20 w-80 p-4"> 
+                <img :src="loan.attachment2 == null ? '' : loan.attachment2" class="w-auto h-auto" >
+            </div>
+                <div class="bg-white shadow-xl rounded-lg content-center lg:ml-20 w-80 p-4"> 
+                <img :src="loan.attachment3 == null ? '' : loan.attachment3" class="w-auto h-auto" >
+            </div>
+        <br>
+        <div v-if="loan.approval == 'For Release'" class="w-full p-0 m-0 flex items-center justify-center gap-20">
+                <button  type="button" class=" py-2 px-4 mb-5 bg-red-600 hover:bg-red-700 focus:ring focus:ring-red-300 text-white w-64 
+                    transition ease-in duration-150 text-lg text-center font-semibold shadow-md rounded-lg"  @click="releasedLoan(loan)">Released</button>
+        </div>
+        </div>
+        
+        <div v-if="loan.approval == 'Released'" class="row-span-3">     
+            <div class="bg-white shadow-xl rounded-lg  content-center lg:ml-20 w-80 h-auto "> 
+                <img :src="loan.attachment1 == null ? '' : loan.attachment1" class="w-auto h-auto" >
+            </div>
+                <div class="bg-white shadow-xl rounded-lg content-center lg:ml-20 w-80 p-4"> 
+                <img :src="loan.attachment2 == null ? '' : loan.attachment2" class="w-auto h-auto" >
+            </div>
+                <div class="bg-white shadow-xl rounded-lg content-center lg:ml-20 w-80 p-4"> 
+                <img :src="loan.attachment3 == null ? '' : loan.attachment3" class="w-auto h-auto" >
+            </div>
+
+        <br>
+        </div>
 
         <div v-if="loan.approval == 'Processed'" class=" w-full p-0 m-0 flex items-center justify-center gap-20">
             
@@ -432,7 +620,6 @@
             </div>
         </div>
     </Modal>        
-</div>
 </AppLayout>
 </template>
 <script>
