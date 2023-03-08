3fe233f39b0777f9a900d773a391f665796f9d66
lourenzs <75241980+lourenzs@users.noreply.github.com>
2023-03-08 22:17:27 +0800

    "mu logo" to "mitrf logo"

--- mapua/resources/js/Jetstream/ApplicationMark.vue
+++ mapua/resources/js/Jetstream/ApplicationMark.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="/images/mu_white.png" alt="mu logo">
+    <img src="/images/mu_white.png" alt="mitrf logo">
 </template>
--- mapua/resources/js/Jetstream/AuthenticationCardLogo.vue
+++ mapua/resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -4,6 +4,6 @@ import { Link } from '@inertiajs/inertia-vue3';
 
 <template>
    <!-- <Link :href="'/'"> -->
-        <img src="/images/mu_white.png" alt="mu logo">
+        <img src="/images/mu_white.png" alt="mitrf logo">
    <!-- </Link> -->
 </template>
