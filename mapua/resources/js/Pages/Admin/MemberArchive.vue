e7d1d37cd84284a8ad2c4b9f787d0512d11940ed
Ryan Villanueva <xxryan0909@gmail.com>
2023-04-14 02:57:36 +0800

    Update MemberArchive.vue

--- mapua/resources/js/Pages/Admin/MemberArchive.vue
+++ mapua/resources/js/Pages/Admin/MemberArchive.vue
@@ -168,6 +168,7 @@
                                                     stroke-width="1.5"
                                                     stroke="green"
                                                     class="w-6 h-6 cursor-pointer"
+                                                    @click="selectedUpdateUser(user)"
                                                 >
                                                     <path
                                                         stroke-linecap="round"
@@ -222,6 +223,123 @@
                 </div>
             </div>
         </div>
+        <Modal
+            :closeable="true"
+            :show="showUpdateModal"
+            @close="showUpdateModal = !showUpdateModal"
+        >
+            <div class="p-5">
+                <div
+                    class="flex justify-between text-xl font-bold text-gray-900 my-3"
+                >
+                    <span>Update User Table</span>
+                    <svg
+                        class="h-6 w-6 cursor-pointer"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        @click="showUpdateModal = !showUpdateModal"
+                    >
+                        <path
+                            d="M6 18L18 6M6 6l12 12"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                        />
+                    </svg>
+                </div>
+                <div
+                    class="flex flex-col items-center text-xl font-bold text-gray-900 my-3"
+                >
+                    <svg
+                        class="h-36 w-36 rounded-full text-red-700 opacity-60"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="1.5px"
+                        version="1.1"
+                        viewBox="0 0 40 40"
+                        xml:space="preserve"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                    >
+                        <g>
+                            <path
+                                d="M17.125,0C7.668,0,0,7.667,0,17.125S7.668,34.25,17.125,34.25c9.459,0,17.125-7.667,17.125-17.125S26.584,0,17.125,0z    M23.445,11.422c1.355,0,2.453,1.099,2.453,2.453s-1.098,2.453-2.453,2.453c-1.354,0-2.451-1.099-2.451-2.453   S22.093,11.422,23.445,11.422z M10.806,11.422c1.354,0,2.453,1.099,2.453,2.453s-1.099,2.453-2.453,2.453s-2.453-1.099-2.453-2.453   S9.453,11.422,10.806,11.422z M26.457,25.641c-0.26,0.492-0.77,0.801-1.328,0.801H9.121c-0.559,0-1.067-0.309-1.328-0.801   c-0.26-0.494-0.225-1.09,0.093-1.549c2.098-3.046,5.551-4.865,9.239-4.865c3.689,0,7.146,1.819,9.24,4.866   C26.684,24.55,26.718,25.146,26.457,25.641z"
+                            />
+                        </g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                        <g></g>
+                    </svg>
+                    <span class="text-center"
+                        >You are about to update {{ userToUpdate.name }}'s
+                        account are you sure you want to proceed?</span
+                    >
+                </div>
+                <div class="flex justify-center">
+                    <div
+                        class="flex justify-between text-xl font-bold dark:text-gray-200 my-3"
+                    >
+                        <div
+                            class="flex space-x-2 mr-5 px-4 py-1 border text-md text-red-600 dark:text-red-600 dark:border-red-600 border-red-600 uppercase rounded-full dark:hover:text-gray-200 hover:text-white hover:border-none hover:bg-red-500 cursor-pointer"
+                            @click="submitUpdateUser"
+                        >
+                            <svg
+                                class="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                />
+                            </svg>
+                            <span>Yes I'm sure</span>
+                        </div>
+                    </div>
+                    <div class="flex text-xl font-bold dark:text-gray-200 my-3">
+                        <div
+                            class="flex space-x-2 px-4 py-1 border text-md text-gray-600 dark:text-gray-600 dark:border-gray-600 border-gray-600 uppercase rounded-full dark:hover:text-gray-200 hover:text-white hover:border-none hover:bg-gray-500 cursor-pointer"
+                            @click="showUpdateModal = !showUpdateModal"
+                        >
+                            <svg
+                                class="h-6 w-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                />
+                            </svg>
+                            <span>Nevermind</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+
 
         <Modal
             :closeable="true"
@@ -371,9 +489,17 @@ export default {
     data() {
         return {
             showDeleteModal: false,
+            showUpdateModal: false,
             userToDelete: Object,
+            userToUpdate: Object,
+
             deleteForm: this.$inertia.form({
                 id: Number,
+            }),
+
+            updateForm: this.$inertia.form({
+                id: Number,
+                status: 1,
             }),
         };
     },
@@ -384,11 +510,27 @@ export default {
             this.showDeleteModal = !this.showDeleteModal;
         },
 
+        selectedUpdateUser(user) {
+            this.userToUpdate = user;
+            this.showUpdateModal = !this.showUpdateModal;
+        },
+
         submitDeleteUser() {
             this.deleteForm.id = this.userToDelete.id;
             this.deleteForm.delete(route("permanentDelete"), {
                 onSuccess: () => {
-                    this.showDeleteModal = false;
+                    this.showUpdateModal = false;
+                },
+            });
+        },
+
+        submitUpdateUser() {
+
+            this.updateForm.id = this.userToUpdate.id;
+            console.log('clicked')
+            this.updateForm.delete(route("userDelete"), {
+                onSuccess: () => {
+                    this.showUpdateModal = false;
                 },
             });
         },
